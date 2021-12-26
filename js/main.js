f48dfebd072ce9cf5adfb4462af0b6e005014fe9
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-26 09:32:51 -0500

    remove console.log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const fig = document.getElementsByTagName("figure");
 for (let i = 0; i < fig.length; i++) {
   const el = fig[i];
-  console.log('click', el.dataset.link);
   el.addEventListener("click", (evt) => {
     window.open(el.dataset.link, "_blank");
   });
